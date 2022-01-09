9cbdf8e13c9480edc440c21e0866bfc75325b36a
wangkai <wangkka1@163.com>
2022-01-09 16:50:28 +0800

    update my project page

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -10,91 +10,153 @@ import styles from "./styles.module.css";
 export default () => {
   return (
     <Layout title="My Project">
-      <div className={styles.container}>
-        <div className={styles.personalCard}>
+      <div style={{ width: "100%", height: "100%", padding: 100 }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            columnGap: 24,
+          }}
+        >
           <img
-            alt="wood3n"
-            src={require("@site/static/img/back.jpg").default}
-            className={styles.backImg}
+            src={require("@site/static/img/happycode.gif").default}
+            style={{
+              flex: "0 0 auto",
+              borderRadius: 8,
+            }}
           />
-          <div style={{ display: "inline-block", margin: "0 160px" }}>
-            <img
-              alt="wood3n"
-              src={require("@site/static/img/avatar.png").default}
-              className={styles.avatar}
-            />
-            <div className={styles.userInfo}>
+          <div style={{ display: "flex", flexDirection: "column", rowGap: 24 }}>
+            <div style={{ position: "relative", left: 24, fontSize: "24px" }}>
+              Hi, I am
+            </div>
+            <img src={require("@site/static/img/sign.png").default} />
+            <div style={{ position: "relative", left: 24, fontSize: "24px" }}>
+              A front end engineer.
+            </div>
+            <div
+              style={{
+                position: "relative",
+                left: 24,
+                display: "flex",
+                columnGap: 24,
+              }}
+            >
               <a
                 target="_blank"
                 href="https://github.com/wood3n"
-                className={styles.information}
+                className={styles.linkIcon}
               >
-                <GitHubIcon />
-                <span className={styles.username}>wood3n</span>
+                <GitHubIcon style={{ width: 24, height: 24 }} />
               </a>
-              <div className={styles.information}>
-                <CareerIcon />
-                <span>Front End Engineer</span>
-              </div>
-
-              <div className={styles.information}>
-                <MailIcon />
-                <span>wangkka1@163.com</span>
-              </div>
-              <div className={styles.information}>
-                <LocationIcon />
-                <span>Hang zhou</span>
-              </div>
-            </div>
-          </div>
-          <div className={styles.skill} style={{ display: "inline-block" }}>
-            <div className={styles.information}>
-              <SkillIcon style={{ width: 30, height: 30 }} />
-              <span>Personal Skills</span>
-            </div>
-            <div className={styles.skillCard}>
-              <span>
-                <img
-                  height="20"
-                  src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"
-                />
-              </span>
-              <span>
-                <img
-                  height="20"
-                  src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png"
-                />
-              </span>
-              <span>
-                <img
-                  height="24"
-                  src={require("@site/static/img/react.png").default}
-                />
-              </span>
-              <span>
-                <img
-                  height="20"
-                  src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png"
-                />
-              </span>
-            </div>
-            <div>
-              <img
-                src="https://camo.githubusercontent.com/794a9aa3668294549a87c7a3277b5b8f6d32632749dd02a3930e74d821fc07ef/68747470733a2f2f6769746875622d726561646d652d73746174732e76657263656c2e6170702f6170692f746f702d6c616e67732f3f757365726e616d653d776f6f64336e266c61796f75743d636f6d70616374267468656d653d7261646963616c"
-                alt="Top Langs"
-                data-canonical-src="https://github-readme-stats.vercel.app/api/top-langs/?username=wood3n&amp;layout=compact&amp;"
-              ></img>
+              <a
+                target="_blank"
+                href="mailto:wangkka1@163.com"
+                className={styles.linkIcon}
+              >
+                <MailIcon style={{ width: 24, height: 24 }} />
+              </a>
             </div>
           </div>
         </div>
-        <div className={styles.projectList}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 80,
+          }}
+        >
+          <h1 style={{ textDecoration: "underline" }}>My Skill</h1>
+        </div>
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            columnGap: 24,
+            marginTop: 80,
+          }}
+        >
+          <img
+            src={require("@site/static/img/typescript.png").default}
+            style={{ height: 100 }}
+          />
+          <img
+            src={require("@site/static/img/react.png").default}
+            style={{ height: 100 }}
+          />
+          <img
+            src={require("@site/static/img/vue.png").default}
+            style={{ height: 100 }}
+          />
+          <img
+            src={require("@site/static/img/nodejs.png").default}
+            style={{ height: 100 }}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 80,
+          }}
+        >
+          <h1 style={{ textDecoration: "underline" }}>My Project</h1>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            rowGap: 48,
+            marginTop: 80,
+          }}
+        >
           <div className={styles.projectCard}>
-            <div></div>
+            <div>
+              <h2>sweet-i18n</h2>
+              <div>
+                根据解析 AST 方案，利用 babel 和 @vue/compiler-sfc 一键提取 vue
+                sfc 中的中文文案，并转换为对应的 vue-i18n
+                的函数，同时生成对应的文案配置 json 文件。
+                真正做到了国际化处理的全自动解决方案。项目地址：——
+                <a target="_blank" href="https://github.com/wood3n/tvt">
+                  sweet-i18n
+                </a>
+              </div>
+            </div>
+            <img src={require("@site/static/img/i18n-en.png").default} />
           </div>
           <div className={styles.projectCard}>
-            <div></div>
+            <div>
+              <h2>node-ts-boilerplate</h2>
+              <div>
+                Nodejs 命令行工具开发模板，使用 esbuild 编译，开发环境可基于
+                vscode 和 ts-node 进行调试。项目地址：——
+                <a target="_blank" href="https://github.com/wood3n/node-ts-cli">
+                  node-ts-boilerplate
+                </a>
+              </div>
+            </div>
+            <img src={require("@site/static/img/nodejs.png").default} />
           </div>
-          <div className={styles.projectCard}></div>
+          <div className={styles.projectCard}>
+            <div>
+              <h2>promise-aplus</h2>
+              <div>
+                基于 ES6 class 实现的 promise A+
+                规范，可通过所有所有官方测试用例。项目地址：——
+                <a
+                  target="_blank"
+                  href="https://github.com/wood3n/promise-aplus"
+                >
+                  promise-aplus
+                </a>
+              </div>
+            </div>
+            <img src={require("@site/static/img/a-plus.png").default} />
+          </div>
         </div>
       </div>
     </Layout>
