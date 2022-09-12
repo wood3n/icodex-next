1076e4eb80f8d9dcf92d560d124e77536c4b07cb
wood3n <wangkka1@163.com>
2022-09-12 17:23:54 +0800

    add blog comment component -- utterances

---
+++ src/theme/BlogPostItem/index.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useRef } from 'react';
+import BlogPostItem from '@theme-original/BlogPostItem';
+import { useColorMode } from '@docusaurus/theme-common';
+import { useBlogPost } from '@docusaurus/theme-common/internal'
+
+export default function BlogPostItemWrapper(props) {
+  const { isBlogPostPage } = useBlogPost();
+  const { isDarkTheme } = useColorMode();
+  const commentContainerRef = useRef();
+
+  const injectScript = () => {
+    const script = document.createElement("script");
+
+    script.src = "https://utteranc.es/client.js";
+    script.setAttribute("repo", "wood3n/icodex-next");
+    script.setAttribute("issue-term", "pathname");
+    script.setAttribute("label", "comment");
+    script.setAttribute("theme", isDarkTheme ? "github-dark" : "github-light");
+    script.crossOrigin = "anonymous";
+    script.async = true;    
+
+    commentContainerRef.current.appendChild(script);
+  }
+
+  const changeCommentTheme = () => {
+    const utterancesEl = commentContainerRef.current.querySelector("iframe.utterances-frame");
+
+    if(utterancesEl) {
+      utterancesEl.contentWindow.postMessage({
+        type: "set-theme",
+        theme: isDarkTheme ? "github-dark" : "github-light",
+      }, "https://utteranc.es");
+    }
+  }
+
+  useEffect(() => {
+    isBlogPostPage && injectScript();
+  }, [])
+
+  useEffect(() => {
+    isBlogPostPage && changeCommentTheme();
+  }, [isDarkTheme])
+
+  return (
+    <>
+      <BlogPostItem {...props} />
+      <div ref={commentContainerRef}></div>
+    </>
+  );
+}
