0b476d087548ae1a7d040093c33096b759e2b76e
oxyg3n <31716713+wood3n@users.noreply.github.com>
2026-08-06 21:41:21 +0800

    docs: fix mcp introduction error

--- sidebars.ts
+++ sidebars.ts
@@ -178,6 +178,10 @@ const sidebars: SidebarsConfig = {
           type: "doc",
           id: "ai/design",
         },
+        {
+          type: "doc",
+          id: "ai/mcp-introduction",
+        },
       ],
     },
   ],
